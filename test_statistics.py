24bd2196829afea4d11a07a7eb4d1677b608152a
inwpolol <Pichaiyut_2001@hotmail.com>
2022-10-12 20:18:42 +0700

    add test stdev.

--- test_statistics.py
+++ test_statistics.py
@@ -1,22 +1,26 @@
 from unittest import TestCase
-from statistics import variance
+from statistics import variance, stdev
 
 
 class StatisticsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_throws_exception(self):
         """variance of an empty list should raise an exception"""
         with self.assertRaises(ValueError):
             var = variance([])
 
+    def test_stdev(self):
+        """standard deviation of typical values"""
+        self.assertEqual(0.0, stdev([10.0, 10.0]))
+        self.assertEqual(1.0, stdev([2, 4]))
+
 
 if __name__ == '__main__':
     import unittest
     unittest.main(verbosity=1)
-
